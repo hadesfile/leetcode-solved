73fd27acfdde70d898489be7706746e0e3e48105
hadesfile <113000376+hadesfile@users.noreply.github.com>
2023-08-07 11:22:19 +0700

    Time: 72 ms (42.71%), Space: 42 MB (59.18%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,47 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    let isCanceled = false;
+
+    const cancelFn = function() {
+        isCanceled = true;
+    };
+
+    setTimeout(() => {
+        if (!isCanceled) {
+            fn(...args);
+        }
+    }, t);
+
+    return cancelFn;
+};
+
+/**
+ *  const result = []
+ *
+ *  const fn = (x) => x * 5
+ *  const args = [2], t = 20, cancelT = 50
+ *
+ *  const start = performance.now() 
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr))
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelT)
+ *           
+ *  setTimeout(() => {
+ *     cancel()
+ *  }, cancelT)
+ *
+ *  setTimeout(() => {
+ *     console.log(result) // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
